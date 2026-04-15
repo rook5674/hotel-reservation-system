dd50592e5cd251044603ca1452f6623d9dae2275
rook5674 <158753510+rook5674@users.noreply.github.com>
2026-04-15 21:46:40 +0200

    created first iteration of user class

---
+++ User.java
@@ -0,0 +1,69 @@
+import java.time.LocalDate;
+
+abstract class User {
+    private String userName;
+    private String password;
+    private LocalDate dateOfBirth;
+    private enum UserType { Staff, GUEST };
+    private UserType type;
+
+    public User(String userName, String password, LocalDate dateOfBirth) {
+        this.userName = userName;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+    public User(){
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public boolean setUserName(String userName, String password) {
+        if (this.password.equals(password)) {
+            this.userName = userName;
+            return true;
+        }
+        else
+            return false;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public boolean setPassword(String password, String oldPassword) {
+        if (this.password.equals(oldPassword)) {
+            this.password = password;
+            return true;
+        }
+        else
+            return false;
+    }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public boolean setDateOfBirth(LocalDate dateOfBirth, String password) {
+        if (this.password.equals(password)) {
+            this.dateOfBirth = dateOfBirth;
+            return true;
+        }
+        else
+            return false;
+    }
+    public UserType getRole() {
+        return type;
+    }
+    public void setRole(UserType type) {
+        this.type = type;
+    }
+    public boolean login(String userName, String password) {
+        if (this.userName.equals(userName) && this.password.equals(password)) {
+            System.out.println("Login successful!");
+            return true;
+        } else {
+            System.out.println("Invalid username or password.");
+            return false;
+        }
+    }
+
+
+ 
+    
+}
