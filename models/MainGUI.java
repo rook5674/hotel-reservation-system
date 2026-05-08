84807fd41a0e27e7d17f569a6de2c5fe11c2ed28
rook5674 <158753510+rook5674@users.noreply.github.com>
2026-05-08 14:35:58 +0300

    fixed maingui name

--- models/MainGUI.java
+++ models/MainGUI.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainUGI extends Application {
+public class MainGUI extends Application {
     
     public static Stage primaryStage;
 
